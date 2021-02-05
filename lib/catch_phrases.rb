f9839f1043c84757e3788fa2a67564ecc52af4b8
Celeste Carter <celesteanell@gmail.com>
2021-02-05 20:48:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,5 +18,5 @@ def catch_phrase
 end
 
 def all_phrases
-  mario = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  toadstool = 
+  mario = ''
+  toadstool = Thank You Mario! But Our Princess Is In Another Castle!
